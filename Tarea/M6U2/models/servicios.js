b275c21171f296ce0c50d20381c188b1a9010fa0
Joaquin Tielas <joaquintielas@gmail.com>
2022-08-24 22:46:26 -0300

    24-8

--- Tarea/M6U2/models/servicios.js
+++ Tarea/M6U2/models/servicios.js
@@ -22,8 +22,42 @@ async function insertServicios(obj) {
         console.log(error);
         throw error;
     }
-
 }
 
-module.exports = { getServicios, insertServicios }
+async function delateServicios(ID) {
+    try {
+        var query = "delete from servicios where ID = ?";
+        var row = await pool.query(query, [ID]);
+        return row;
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getServiciosbyID(ID) {
+    try {
+        var query = "select * from servicios where ID = ?";
+        var rows = await pool.query(query, [ID]);
+        return rows;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function editServiciosbyID(obj, ID) {
+    try {
+        var query = "update servicios set ? where ID=?";
+        var rows = await pool.query(query, [obj, ID])
+        return rows;
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = { getServicios, insertServicios, delateServicios, getServiciosbyID, editServiciosbyID }
 
--- Tarea/M6U2/routes/admin.js
+++ Tarea/M6U2/routes/admin.js
@@ -4,16 +4,49 @@ var getServicios = require('../models/servicios')
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
-
   var servicios = await getServicios.getServicios();
-
+  console.log(servicios)
   res.render('admin', {
     nombre: req.session.user,
     servicios: servicios
   });
 });
 
-router.get('/agregar', (req, res)=>{
+
+router.get('/eliminar/:ID', async (req, res, next) => {
+  var ID = req.params.ID;
+  await getServicios.delateServicios(ID);
+  res.redirect('/admin')
+});
+
+
+router.get('/editar/:ID', async (req, res, next) => {
+  var ID = req.params.ID;
+  var datos = await getServicios.getServiciosbyID(ID);
+  console.log(datos)
+  res.render('editar', {
+    datos
+  });
+});
+
+router.post('/editar/:ID', async (req, res, next) => {
+  try {
+    var obj = {
+      ID: req.body.ID,
+      Servicio: req.body.Servicio,
+      Descripcion: req.body.Descripcion,
+      Imagen: req.body.Imagen
+    }
+    await getServicios.editServiciosbyID(obj, req.body.ID)
+    res.redirect('/admin');
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  };
+});
+
+router.get('/agregar', (req, res) => {
   res.render('agregar');
 })
 
